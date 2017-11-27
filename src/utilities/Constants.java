c182c7712d125ffad1283acca40176ce378ff241
dominictd92 <dominictd92@gmail.com>
2017-11-27 02:28:37 -0500

    Creation of basic files
    Added the Constants file and layout of the home screen

---
+++ src/utilities/Constants.java
@@ -0,0 +1,19 @@
+package utilities;
+
+import java.awt.Font;
+
+public class Constants {
+	public final static int WINDOW_HEIGHT = 500;
+	public final static int WINDOW_WIDTH = 500;
+	
+	//Home Constants
+	public final static String HOME = "Home";
+	
+	//Headers
+	public final static Font HEADER_FONT = new Font("Courier", Font.BOLD, 20);
+	public final static int PAGE_TITLE_WIDTH = WINDOW_WIDTH/2;
+	public final static int PAGE_TITLE_HEIGHT = 50;
+	
+	public final static int BUTTON_WIDTH = 100;
+	public final static int BUTTON_HEIGHT = 50;
+}
