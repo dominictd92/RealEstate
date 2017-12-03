46b0e3da269d3cc2992a0c7a236a3ba9c2210383
dominictd92 <dominictd92@gmail.com>
2017-12-02 22:17:13 -0500

    Fixed Deletion file
    Fixed deletion file

--- tableDeletion.sql
+++ tableDeletion.sql
@@ -1,20 +1,20 @@
-DROP TABLE State
-DROP TABLE Dependent_Lives_with_Tennant
-DROP TABLE Property_has_Expense
-DROP TABLE Property_has_Amenity
-DROP TABLE Property_Needs_Repair
-DROP TABLE Applicant_Applies_For_Property
-DROP TABLE Seller_Sells_Property
-DROP TABLE Employee_Works_At_Property
-DROP TABLE Property
-DROP TABLE Expense
-DROP TABLE Amenity
-DROP TABLE Employee
-DROP TABLE Applicant
-DROP TABLE Tennant
-DROP TABLE Dependent
-DROP TABLE Seller
-DROP TABLE Repair
+DROP TABLE State;
+DROP TABLE Dependent_Lives_with_Tennant;
+DROP TABLE Property_has_Expense;
+DROP TABLE Property_has_Amenity;
+DROP TABLE Property_Needs_Repair;
+DROP TABLE Applicant_Applies_For_Property;
+DROP TABLE Seller_Sells_Property;
+DROP TABLE Employee_Works_At_Property;
+DROP TABLE Property;
+DROP TABLE Expense;
+DROP TABLE Amenity;
+DROP TABLE Employee;
+DROP TABLE Applicant;
+DROP TABLE Tennant;
+DROP TABLE Dependent;
+DROP TABLE Seller;
+DROP TABLE Repair;
 
 
 
