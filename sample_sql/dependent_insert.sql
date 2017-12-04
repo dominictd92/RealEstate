e9d3573da83deb1c9f892be3bbe2a7cfd1ca368c
ahead01 <ahdase@gmail.com>
2017-12-04 09:49:52 -0500

    dependert and expense insert ** Used a special standard for the cost col in expense so that it fit with the original project prpopsal * FIxed costs have a cost of 1 and General have a cost of 2 * we can say that this is to be used to calculate cost

---
+++ sample_sql/dependent_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `RealEstate`.`Dependent` (`depId`, `f_name`, `m_name`, `l_name`, `birthDate`) VALUES ('1', 'Alex', 'D', 'Smith', '2000-01-01 00:00:00');
+INSERT INTO `RealEstate`.`Dependent` (`depId`, `f_name`, `m_name`, `l_name`, `birthDate`) VALUES ('2', 'Jane', 'D', 'Routh', '2001-01-01 00:00:00');
+INSERT INTO `RealEstate`.`Dependent` (`depId`, `f_name`, `m_name`, `l_name`, `birthDate`) VALUES ('3', 'John', 'D', 'Davis', '2002-01-01 00:00:00');
+INSERT INTO `RealEstate`.`Dependent` (`depId`, `f_name`, `m_name`, `l_name`, `birthDate`) VALUES ('4', 'Ruth', 'D', 'Doe', '2003-01-01 00:00:00');
---
+++ sample_sql/expense_insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('1', '1', 'Property Managment Fee');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('2', '1', 'Gas & Electric');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('3', '1', 'Water');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('4', '1', 'Landscaping');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('5', '1', 'Property Taxes');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('6', '1', 'Insurance');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('7', '1', 'Rental License');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('8', '1', 'P.O. Box');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('9', '2', 'Contractor');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('10', '2', 'Plumber');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('11', '2', 'Electrician');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('12', '2', 'Painter');
+INSERT INTO `RealEstate`.`Expense` (`expenseId`, `cost`, `expense`) VALUES ('13', '2', 'Handyman');
+
+-- NOTE ** THe Cost column is really a 'factor constant' to be used in calculation
+-- For example: all fixed costs factor's are 1 so they do not change
