53277c880d196059e8bd7fe72d4435379c0a01a0
ahead01 <ahdase@gmail.com>
2017-12-04 10:09:21 -0500

    repait and tennant insert

---
+++ sample_sql/repair_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `RealEstate`.`Repair` (`repairId`, `cost`, `effectedArea`, `vendor`) VALUES ('1', '7000', 'yard', 'county');
+INSERT INTO `RealEstate`.`Repair` (`repairId`, `cost`, `effectedArea`, `vendor`) VALUES ('2', '10', 'mailbox', 'lowes');
+INSERT INTO `RealEstate`.`Repair` (`repairId`, `cost`, `effectedArea`, `vendor`) VALUES ('3', '100', 'garage-door', 'lowes');
+INSERT INTO `RealEstate`.`Repair` (`repairId`, `cost`, `effectedArea`, `vendor`) VALUES ('4', '5000', 'concrete', 'BIG');
