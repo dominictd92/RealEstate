f85f33fe15091f23e34be53990695ba5a1e7035c
ahead01 <ahdase@gmail.com>
2017-12-04 11:23:42 -0500

    Create ammenity_insert.sql

---
+++ sample_sql/ammenity_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO `RealEstate`.`Amenity` (`amenityid`, `amenity`) VALUES ('1', 'amenity1');
+INSERT INTO `RealEstate`.`Amenity` (`amenityid`, `amenity`) VALUES ('2', 'amenity2');
+INSERT INTO `RealEstate`.`Amenity` (`amenityid`, `amenity`) VALUES ('3', 'amenity3');
+INSERT INTO `RealEstate`.`Amenity` (`amenityid`, `amenity`) VALUES ('4', 'amenity4');
