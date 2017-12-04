c0cbee9f0a48cafb91baae61f486467412a7eb4b
ahead01 <ahdase@gmail.com>
2017-12-04 11:25:46 -0500

    Create property_has_amenity.sql

---
+++ sample_sql/property_has_amenity.sql
@@ -0,0 +1,9 @@
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('1', '1', '1');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('2', '2', '2');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('3', '3', '3');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('4', '4', '4');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('2', '5', '5');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('3', '6', '6');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('4', '7', '7');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('3', '8', '8');
+INSERT INTO `RealEstate`.`Property_has_Amenity` (`Amenity_amenityid`, `Property_propertyID`, `Porporty_amenityid`) VALUES ('4', '9', '9');
